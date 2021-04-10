62ccbefa472d6d29ab11c7107df8ae083431c2a2
justinanmoreno <justinanmoreno@gmail.com>
2021-04-10 18:09:57 -0400

    Backend boilerplate/setup. Version1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { useState,useEffect } from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
+}
+
+async function getAppData() {
+  const BASE_URL = "http://localhost:3001/api";
+  const entries = await fetch(BASE_URL).then(res => res.json());
+  setState((prevState) => ({
+    ...prevState, 
+    entries,  
+  }));  
+};
+
+useEffect(() => {
+  getAppData();
+}, []);
+
+async function addEntry(e) {
+  e.preventDefault();
+
+  const BASE_URL = "http://localhost:3001/api";
+
+  const entry = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-type": "Applications/json"
+    },
+    body: JSON.stringify(state.newEntry)
+  }).then(res => res.json());
+
+  setState((prevState) => ({
+    entries: [...prevState.entries, entry],
+    newEntry: {
+      entry: "",
+    },
+  }));
 }
 
 export default App;
